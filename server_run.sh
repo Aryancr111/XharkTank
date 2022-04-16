7fe37c9f5d71d30eef1fb1203b03ea818b4ddf58
rex lucifer <rexlucifer111@gmail.com>
2022-04-16 20:32:45 +0530

    changed eol

--- server_run.sh
+++ server_run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# This script must contain a series of commands to run the backend application
-# Eg:- For Node.js project;
-#       npm run start
-# Eg:- For SpringBoot Project;
 #End of line changed to LF
 ./gradlew bootRun
-# Eg:- For Python Flask Project
-#       export FLASK_APP=main.py
-#       flask run
